45a00befcb80b6042d355f89fd651459e048f4ef
luhuadong <luhuadong@163.com>
2020-04-22 00:26:14 +0800

    remove user macro

--- inc/bc28_mqtt.h
+++ inc/bc28_mqtt.h
@@ -11,13 +11,6 @@
 #ifndef __AT_BC28_H__
 #define __AT_BC28_H__
 
-//#define JSON_DATA_PACK_TEST      "{\"id\":\"125\",\"version\":\"1.0\",\"params\":{\"Temp\":%s,\"Humi\":%s,\"Dust\":%s,\"TVOC\":%s,\"eCO2\":%s},\"method\":\"thing.event.property.post\"}\x1A"
-//#define JSON_DATA_PACK           "{\"id\":\"125\",\"version\":\"1.0\",\"params\":{\"Temp\":%d.%02d,\"Humi\":%d.%02d,\"Dust\":%d,\"TVOC\":%d,\"eCO2\":%d},\"method\":\"thing.event.property.post\"}\x1A"
-#define JSON_DATA_PACK_STR       "{\"id\":\"125\",\"version\":\"1.0\",\"params\":{\"Temp\":%s,\"Humi\":%s,\"Dust\":%d,\"TVOC\":%d,\"eCO2\":%d},\"method\":\"thing.event.property.post\"}\x1A"
-//#define AIR_MSG                  "[Air] Temp: %d.%02d'C, Humi: %d.%02d%, Dust: %dug/m3, TVOC: %dppb, eCO2: %dppm\n"
-#define MQTT_TOPIC_HELLO         "/a1p8Pngb3oY/BC28/user/hello"
-#define MQTT_TOPIC_UPLOAD        "/sys/a1p8Pngb3oY/BC28/thing/event/property/post"
-
 typedef enum {
 
 	AT_STAT_INIT = 0,
