0b7253ff68cb567ea227e1f65b76632c6eb979d7
luhuadong <luhuadong@163.com>
2020-08-16 12:46:02 +0800

    add json parse example

--- examples/bc28_mqtt_sample.c
+++ examples/bc28_mqtt_sample.c
@@ -6,29 +6,75 @@
  * Change Logs:
  * Date           Author       Notes
  * 2020-04-22     luhuadong    the first version
+ * 2020-08-16     luhuadong    add json parse
  */
 
 #include <rtthread.h>
 #include <bc28_mqtt.h>
+#include <cJSON.h>
+
+
+/* Alink JSON example
+
+{
+    "method":"thing.service.property.set",
+    "id":"1686377270",
+    "params":
+    {
+        "powerstate":1
+    },
+    "version":"1.0.0"
+}
+
+*/
+static void mqtt_recv_cb(const char *json)
+{
+    cJSON *obj = cJSON_Parse(json);
+
+    cJSON *powerstate = cJSON_GetObjectItem(cJSON_GetObjectItem(obj, "params"), "powerstate");
+
+    if (powerstate->valueint == 1)
+    {
+        //light_on();
+        rt_kprintf("switch on");
+    }
+    else if (powerstate->valueint == 0)
+    {
+        //light_off();
+        rt_kprintf("switch off");
+    }
+}
 
 static void bc28_mqtt_sample(void *parameter)
 {
-    if(RT_EOK != bc28_init())
+    /* init bc28 model */
+    if (bc28_init() < 0)
     {
-        rt_kprintf("init failed\n");
+        rt_kprintf("(BC28) init failed\n");
         return;
     }
-    rt_kprintf("attach ok\n");
 
-    while (RT_EOK != build_mqtt_network())
+    /* attach network */
+    if (bc28_client_attach() < 0)
     {
-        bc28_mqtt_close();
-        rt_kprintf("rebuild mqtt network\n");
+        rt_kprintf("(BC28) attach failed\n");
+        return;
     }
 
-    rt_kprintf("MQTT connect ok\n");
+    rt_kprintf("(BC28) attach ok\n");
 
-    /* user-code */
+    /* build mqtt network */
+    while (bc28_build_mqtt_network() < 0)
+    {
+        bc28_mqtt_close();
+        rt_kprintf("(BC28) rebuild mqtt network\n");
+    }
+    rt_kprintf("(BC28) MQTT connect ok\n");
+
+    /* bind parser callback function */
+    bc28_bind_parser(mqtt_recv_cb);
+
+    /* subscribe a topic */
     char topic[256];
     rt_sprintf(topic, "%s/%s/user/hello", 
                PKG_USING_BC28_MQTT_PRODUCT_KEY, 
@@ -39,6 +85,7 @@ static void bc28_mqtt_sample(void *parameter)
     int cnt = 10;
     while (cnt--)
     {
+        /* publish a topic */
         bc28_mqtt_publish(topic, "Hello, World!");
         rt_thread_mdelay(2000);
     }
